f919de54f3d986b11b78c0b52e6e0ab45a0b667a
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-01-22 13:34:28 +0100

    add get atribbute for clicked element, find article and show article

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 "use strict";
-/*
-document.getElementById("test-button").addEventListener("click", function () {
-  const links = document.querySelectorAll(".titles a");
-  console.log("links", links);
-});
-*/
+
 const titleClickHandler = function () {
+  event.preventDefault();
+  const clickedElement = this;
   console.log("Link was clicked");
   console.log(event);
 
-  /* remove class 'active' from all articles link */
+  /* [DONE] remove class 'active' from all articles link */
   const activeLinks = document.querySelectorAll(".titles a.active");
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log("clicked element", clickedElement);
+  clickedElement.classList.add("active");
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll("article.active");
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
   }
 
-  /* remove class 'active' from all articles */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const activeSelector = clickedElement.getAttribute("href");
+  console.log(activeSelector);
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const findArticle = document.querySelector("article" + activeSelector);
+  console.log(findArticle);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  findArticle.classList.add("active");
 };
 
 const links = document.querySelectorAll(".titles a");
